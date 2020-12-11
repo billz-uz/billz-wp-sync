e6874a8aad128f88bb8e70731c312f7654d8e16c
Marat Kharbiyanov <kharbiyanov@gmail.com>
2020-12-12 00:51:47 +0500

    added github-updater support

--- billz-wp-sync.php
+++ billz-wp-sync.php
@@ -22,6 +22,7 @@
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       billz-wp-sync
+ * GitHub Plugin URI: https://github.com/afragen/github-updater
  * Domain Path:       /languages
  */
 
